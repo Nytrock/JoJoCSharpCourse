931bf47a59b1df8dfa28abd03afe7f470b05e58f
Nytrock <savvazhirnov@gmail.com>
2023-09-27 22:03:52 +0300

    First section completed

---
+++ 1 - Introduction/1.2.2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._2._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Привет, Спидвагон! Мне нужна твоя помощь.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 1 - Introduction/1.2.3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._2._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("His name\'s...Diavolo!");
+            Console.ReadKey();
+        }
+    }
+}
