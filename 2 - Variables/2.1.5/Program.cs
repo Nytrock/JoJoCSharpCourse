d4e045bd216ed89d65d58ca67d0932114aa2d2ce
Nytrock <savvazhirnov@gmail.com>
2023-09-28 18:24:17 +0300

    Second section completed

---
+++ 2 - Variables/2.1.5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2._1._5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            Console.WriteLine($"Аривидерчи, {name}. Я уезжаю в лучшую жизнь");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 2 - Variables/2.1.6/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2._1._6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = Console.ReadLine();
+            string secondName = Console.ReadLine();
+            Console.WriteLine("Бон Жорно, как вас двоих зовут?");
+            Console.WriteLine(firstName);
+            Console.WriteLine(secondName);
+            Console.WriteLine($"Теперь я {secondName} {firstName}! Чао!");
+            Console.ReadKey();
+        }
+    }
+}
